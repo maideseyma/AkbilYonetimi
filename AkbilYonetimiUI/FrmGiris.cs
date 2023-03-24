30d619bcc8ac282d56802a8c5eb2c4df7a7177b4
maideseyma <maideseymaersahan@gmail.com>
2023-03-24 15:52:58 +0300

    Add project files.

---
+++ AkbilYonetimiUI/FrmGiris.cs
@@ -0,0 +1,24 @@
+﻿namespace AkbilYonetimiUI
+{
+    public partial class FrmGiris : Form
+    {
+        public FrmGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmGiris_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            // Bu formu gizleyeceğiz.
+            // Kayıt ol formunu açacağız.
+            this.Hide();
+            FrmKayitOl frm = new FrmKayitOl();
+            frm.Show();
+        }
+    }
+}
---
+++ AkbilYonetimiUI/FrmKayitOl.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AkbilYonetimiUI
+{
+    public partial class FrmKayitOl : Form
+    {
+        public FrmKayitOl()
+        {
+            InitializeComponent(); // İnşa etmek
+        }
+
+        private void FrmKayitOl_Load(object sender, EventArgs e)
+        {
+            #region Ayarlar
+            txtSifre.PasswordChar = '*';
+            dtpDogunTarihi.MaxDate = new DateTime(2016, 1, 1);
+            dtpDogunTarihi.Value = new DateTime(2016, 1, 1);
+            dtpDogunTarihi.Format = DateTimePickerFormat.Short;
+
+            #endregion
+        }
+
+        private void btnKayitOl_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //1) Emailden kayıtlı biri zaten var mı? 
+                string baglantiCumlesi = @"Server=DESKTOP-E30TBPJ\MSSQLSERVER01;Database=AKBILDB;Trusted_Connection=True;";
+
+                SqlConnection baglanti = new SqlConnection(); // baglanti nesnesi
+                baglanti.ConnectionString = baglantiCumlesi; // nereye bağlanacak ?
+                SqlCommand komut = new SqlCommand(); //komut nesnesi türettik
+                komut.Connection = baglanti; // komutun hangi bağlantıda çalışacağını atadık.
+                komut.CommandText = $"select * from Kullanicilar (nolock) where Email='{txtEmail.Text.Trim()}'"; // sql komutu
+                baglanti.Open();
+
+                SqlDataReader okuyucu = komut.ExecuteReader(); // çalıştır
+                if (okuyucu.HasRows) // Satır var mı?
+                {
+                    MessageBox.Show("Bu email zaten sisteme kayıtlıdır!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+                baglanti.Close();
+
+                //2) Emaili daha önce kayıtlı değilse KAYIT OLACAK
+                if (string.IsNullOrEmpty(txtIsım.Text) || string.IsNullOrEmpty(txtSoyisim.Text) || string.IsNullOrEmpty(txtEmail.Text) || string.IsNullOrEmpty(txtSifre.Text))
+                {
+                    MessageBox.Show("Bilgileri eksiksiz giriniz !", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+                string insertSQL = $"insert into Kullanicilar (EklenmeTarihi,Email,Parola,Ad,Soyad,DogumTarihi) values " +
+                    $"('{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}','{txtEmail.Text.Trim()}','{txtSifre.Text.Trim()}','{txtIsım.Text.Trim()}','{txtSoyisim.Text.Trim()}','{dtpDogunTarihi.Value.ToString("yyyyMMdd")}')";
+                baglanti.ConnectionString = baglantiCumlesi;
+                SqlCommand eklemeKomut = new SqlCommand(insertSQL, baglanti);
+                baglanti.Open();
+                int rowsEffected = eklemeKomut.ExecuteNonQuery(); // insert update delete için kullanılır.
+                if (rowsEffected > 0)
+                {
+                    MessageBox.Show("KAYIT EKLENDİ !", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("KAYIT EKLENEMEDİ !", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                baglanti.Close();
+                //Temizlik gerekli
+
+            }
+            catch (Exception ex)
+            {
+                // ex log.txt'ye yazılanacak (loglama)
+                MessageBox.Show("Beklenmedik bir hata oluştu! Lütfen tekrar deneyiniz !");
+            }
+        }
+    }
+}
